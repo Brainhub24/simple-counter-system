d256b6ab6d17317c6ef7d7f54c0bb52f8120d19b
Jason Prime <600613.m6@gmail.com>
2023-01-14 23:45:04 +0100

    Some improvements & issue bugfixes
    - Regenerate session ID on every request to prevent session fixation attacks.
    - Remove session data when user closes browser
    - Usage of a secure connection (HTTPS) if possible
    - Use HttpOnly for session cookies
    - Validate user input using filter_var function, to ensure that the data is a valid number
    - Set a default value to the session variable if it's not set or not a valid number

--- simple-counter.php
+++ simple-counter.php
@@ -1,9 +1,36 @@
 <?php
+/*
+  A simple php counter snippet
+  - - - - - - - - - - - - - - - 
+  https://github.com/Brainhub24/simple-counter-system
+  Contact: github@brainhub24.com
+*/
 session_start();
-if(!isset($_SESSION['visits'])){
-    $_SESSION['visits'] = 0;
-}
-$_SESSION['visits']++;
 
-echo "You have visited this page " . $_SESSION['visits'] . " times.";
+// Regenerate session ID on every request to prevent session fixation attacks
+session_regenerate_id();
+
+// validate user input
+$visits = filter_var($_WEBSERVICE_SESSION['visits'], FILTER_VALIDATE_INT);
+
+// if visits is not set or not a valid number, set it to 0
+if(!$visits){
+    $visits = 0;
+}
+
+$visits++;
+
+// Remove session data when user closes browser
+session_set_cookie_params(0, '/', '', true, true);
+
+// Use of a secure connection (HTTPS) if possible 
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+    ini_set('session.cookie_secure', 1);
+}
+
+// Use HttpOnly for session cookies
+ini_set('session.cookie_httponly', 1);
+
+$_WEBSERVICE_SESSION['visits'] = $visits;
+echo "You have visited this page " . $_WEBSERVICE_SESSION['visits'] . " times.";
 ?>
